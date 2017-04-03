24762d41eccc2175ef303cabe58d71f8a9c4272a
Austin May <austinmay2460@gmail.com>
2017-04-02 20:55:01 -0400

    Add middle name

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -11,6 +11,7 @@ namespace TeamPowered
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string MiddleName { get; set; }
         public string Address { get; set; }
     }
 }
